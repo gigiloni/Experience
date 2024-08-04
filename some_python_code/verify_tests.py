8b4cb252a88d64df684c62d1d65138544b463dc5
Georgii Tevzadze <georgii.tevzadze05@gmail.com>
2024-08-04 11:38:21 +0200

    adding a new Test in verify_tests.py

--- some_python_code/verify_tests.py
+++ some_python_code/verify_tests.py
@@ -24,7 +24,7 @@ class Verify:
         return f'The result of {self.test_name} is {cor_answers}/{total_q}'
 
 
-class Test(Verify):
+class Test1(Verify):
     def __init__(self, right_answers, answers, test_name=""):
         super().__init__(right_answers, answers, test_name)
         self.verify_answers(self.right_answers, self.answers)
@@ -33,6 +33,19 @@ class Test(Verify):
         EX_10 = {'iteration', 'Iteration', 'ITERATION'}
         count = super().verify_answers(r_ans, ans)
         if ans.get('10 ex') in EX_10:
+            count += 1
+        return count
+
+
+class Test2(Verify):
+    def __init__(self, right_answers, answers, test_name=""):
+        super().__init__(right_answers, answers, test_name)
+        self.verify_answers(self.right_answers, self.answers)
+
+    def verify_answers(self, r_ans: dict, ans: dict):
+        EX_11 = {'left', 'Left', "LEFT"}
+        count = super().verify_answers(r_ans, ans)
+        if ans.get('11 ex') in EX_11:
             count += 1
         return count
 
@@ -60,6 +73,19 @@ r1_answers = {
     '8 ex': 'b',
     '9 ex': 'b',
     '10 ex': 'iteration'
+}
+r2_answers = {
+    '1 ex': 'd',
+    '2 ex': 'c',
+    '3 ex': 'a',
+    '4 ex': 'b',
+    '5 ex': 'd',
+    '6 ex': 'b',
+    '7 ex': 'c',
+    '8 ex': 'a',
+    '9 ex': 'a',
+    '10 ex': 'c',
+    '11 ex': 'left'
 }
 s3_answers = {
     '1 ex': 'b',
@@ -100,7 +126,7 @@ s2_answers = {
 
 s1 = Verify(right_answers=r_answers, answers=s1_answers, test_name='Alice Smith')
 s2 = Verify(right_answers=r_answers, answers=s2_answers, test_name='George Soul')
-s3 = Test(r1_answers, s3_answers, 'Claus Wrong')
+s3 = Test1(r1_answers, s3_answers, 'Claus Wrong')
 print(s1)
 print(s2)
 print(s3)
